06f11917c7a20e84905f02ec4569a0bdd173b50d
= <=>
2025-06-08 12:31:25 +0200

    Pipes personalizados con Angular 19

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/canFly.pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero.filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    TitleCasePipe,
+    ToggleCasePipe,
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe,
+    HeroCreatorPipe,
+    HeroSortByPipe,
+    HeroFilterPipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent {}
+export default class CustomPageComponent {
+  name = signal('José Alfonso');
+  upperCase = signal(true);
+
+  heroes = signal(heroes);
+
+  sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('');
+}
